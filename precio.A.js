13a90771f3dad6bc844a4006df41637383971bba
Vicente <viccenzomiranda@gmail.com>
2025-06-16 21:49:41 -0300

    todo

---
+++ precio.A.js
@@ -0,0 +1,53 @@
+function calcular(){
+    let op1=document.getElementById("equipos").value;
+    let op2 = document.querySelector('input[name="talle"]:checked').value;
+
+    if(op1=="boca" && op2=="s"){
+        document.getElementById("resultado2").textContent="$25.000$"}
+    if(op1=="boca" && op2=="m"){
+        document.getElementById("resultado2").textContent="$25.500$"}
+    if(op1=="boca" && op2=="l"){
+    document.getElementById("resultado2").textContent="$26.000$"}
+    if(op1=="river" && op2=="s"){
+        document.getElementById("resultado2").textContent="$25.000$"}
+    if(op1=="river" && op2=="m"){
+        document.getElementById("resultado2").textContent="$25.500$"}
+    if(op1=="river" && op2=="l"){
+    document.getElementById("resultado2").textContent="$26.000$"}
+    if(op1=="racing" && op2=="s"){
+        document.getElementById("resultado2").textContent="$23.000$"}
+    if(op1=="racing" && op2=="m"){
+        document.getElementById("resultado2").textContent="$23.500$"}
+    if(op1=="racing" && op2=="l"){
+    document.getElementById("resultado2").textContent="$24.000$"}
+    if(op1=="independiente" && op2=="s"){
+        document.getElementById("resultado2").textContent="$23.000$"}
+    if(op1=="independiente" && op2=="m"){
+        document.getElementById("resultado2").textContent="$23.500$"}
+    if(op1=="independiente" && op2=="l"){
+    document.getElementById("resultado2").textContent="$24.000$"}
+    if(op1=="barcelona" && op2=="s"){
+        document.getElementById("resultado2").textContent="$27.000$"}
+    if(op1=="barcelona" && op2=="m"){
+        document.getElementById("resultado2").textContent="$27.500$"}
+    if(op1=="barcelona" && op2=="l"){
+    document.getElementById("resultado2").textContent="$28.000$"}
+    if(op1=="realmadrid" && op2=="s"){
+        document.getElementById("resultado2").textContent="$27.000$"}
+    if(op1=="realmadrid" && op2=="m"){
+        document.getElementById("resultado2").textContent="$27.500$"}
+    if(op1=="realmadrid" && op2=="l"){
+    document.getElementById("resultado2").textContent="$28.000$"}
+    if(op1=="manchestercity" && op2=="s"){
+        document.getElementById("resultado2").textContent="$27.000$"}
+    if(op1=="manchestercity" && op2=="m"){
+        document.getElementById("resultado2").textContent="$27.500$"}
+    if(op1=="manchestercity" && op2=="l"){
+    document.getElementById("resultado2").textContent="$28.000$"}
+    if(op1=="psg" && op2=="s"){
+        document.getElementById("resultado2").textContent="$27.000$"}
+    if(op1=="psg" && op2=="m"){
+        document.getElementById("resultado2").textContent="$27.500$"}
+    if(op1=="psg" && op2=="l"){
+    document.getElementById("resultado2").textContent="$28.000$"}
+}
